0fd0a24c15a7670ae2d0bd7a588cd8c51e87e026
fedecech <federicocech@gmail.com>
2023-02-13 17:21:52 +0000

    solved lab

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -8,24 +8,26 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.io.IOException;
 
 @RestController
-public class PingPongEndpoint {    
+public class PingPongEndpoint {
 
-	GRPCClientService grpcClientService;    
-	@Autowired
-    	public PingPongEndpoint(GRPCClientService grpcClientService) {
-        	this.grpcClientService = grpcClientService;
-    	}    
-	@GetMapping("/ping")
-    	public String ping() {
-        	return grpcClientService.ping();
-    	}
-        @GetMapping("/add")
-	public String add() {
-		return grpcClientService.add();
-	}
+  GRPCClientService grpcClientService;
+  @Autowired
+  public PingPongEndpoint(GRPCClientService grpcClientService) {
+    this.grpcClientService = grpcClientService;
+  }
+  @GetMapping("/ping")
+  public String ping() {
+    return grpcClientService.ping();
+  }
+  @GetMapping("/add")
+  public String add() {
+    return grpcClientService.add();
+  }
+
+  @GetMapping("/mult")
+  public String mult() {
+    return grpcClientService.mult();
+  }
 }
