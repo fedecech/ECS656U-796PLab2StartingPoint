2f39c08e2faec2ced6b89a63871cbe04f6449965
fedecech <federicocech@gmail.com>
2023-02-13 17:36:42 +0000

    add import

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+
 @RestController
 public class PingPongEndpoint {
 
