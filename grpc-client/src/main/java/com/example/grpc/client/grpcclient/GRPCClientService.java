2de5a786070c9905911e3c42d7500214cd3dcffc
fedecech <federicocech@gmail.com>
2023-02-13 18:40:26 +0000

    quiz q7

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -62,8 +62,7 @@ public class GRPCClientService {
                                        .setB11(6)
                                        .build());
 
-    String resp = A.getC00() + " " + A.getC01() + "<br>" + A.getC10() + " " +
-                  A.getC11() + "\n";
+    String resp= A.getC00()+A.getC01()+A.getC10()+A.getC11()+"";
     return resp;
   }
 }
